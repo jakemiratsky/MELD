ad75bfc926f8004bda27f46fb95bef1c21cfa317
Jacob Miratsky <98666696+jakemiratsky@users.noreply.github.com>
2022-03-23 19:45:06 -0800

    Update setup_MELD.py

--- setup_MELD.py
+++ setup_MELD.py
@@ -51,6 +51,56 @@ def make_cartesian_collections(s, scaler, residues, delta=0.2, k=250.):
             cart.append(rest)
     return cart
 
+# Now what we want is to put restraints on the residue dinstance values 
+
+def get_dist_restraints(filename, s, scaler):             # to read the binding restraints
+    dists = []
+    rest_group = []
+    lines = open(filename).read().splitlines()
+    lines = [line.strip() for line in lines]
+    for line in lines:
+        if not line:
+            dists.append(s.restraints.create_restraint_group(rest_group, 1))                    # enforcing 1 restraints from each group
+            rest_group = []
+        else:
+            cols = line.split()
+            i = int(cols[0])
+            name_i = cols[1]
+            j = int(cols[2])
+            name_j = cols[3]
+            dist = float(cols[4])                          # MELD uses nm unit for distance
+
+            rest = s.restraints.create_restraint('distance', scaler,LinearRamp(0,100,0,1),       #Flatbottom harmonic restraints with no poteintial from 0 nm (r2) to 'dist' (r3) in the given in the file and then r3 to r4 increaing harmonically and after that increasing lineraly with k=350 kJ/(mol.nm*2)
+                                              r1=0.0, r2=0.0, r3=dist, r4=dist+0.2, k=350,
+                                              atom_1_res_index=i, atom_2_res_index=j,
+                                              atom_1_name=name_i, atom_2_name=name_j)
+            rest_group.append(rest)
+return dists
+
+
+def get_dist_restraints_protein(filename, s, scaler):                   #To read the restraint to keep protein conformation fixed
+    dists = []
+    rest_group = []
+    lines = open(filename).read().splitlines()
+    lines = [line.strip() for line in lines]
+    for line in lines:
+        if not line:
+            dists.append(s.restraints.create_restraint_group(rest_group, 1))
+            rest_group = []
+        else:
+            cols = line.split()
+            i = int(cols[0])
+            name_i = cols[1]
+            j = int(cols[2])
+            name_j = cols[3]
+            dist = float(cols[4])
+
+            rest = s.restraints.create_restraint('distance', scaler,LinearRamp(0,100,0,1),
+                                              r1=dist-0.2, r2=dist-0.1, r3=dist+0.1, r4=dist+0.2, k=350,      # here we have 0 energy penalty in betwen dist-0.1 and  dist+0.1 region making it stronger contact.
+                                              atom_1_res_index=i, atom_2_res_index=j,
+                                              atom_1_name=name_i, atom_2_name=name_j)
+            rest_group.append(rest)
+return dists
 
 def setup_system():
     #print "!!!Starting from Templates!!!"
@@ -60,15 +110,27 @@ def setup_system():
     p = system.ProteinMoleculeFromPdbFile(templates[0])
     b = system.SystemBuilder(forcefield="ff14sbside")
     s = b.build_system_from_molecules([p])
-    s.temperature_scaler = system.GeometricTemperatureScaler(0, 0.4, 300, 500.)
+    s.temperature_scaler = system.GeometricTemperatureScaler(0, 0.4, 300, 350.)
     n_res = s.residue_numbers[-1]
 
-    const_scaler = s.restraints.create_scaler('constant')
-    pro_res = list(range(26,91)) + list(range(121,151))+list(range(154,163))+list(range(165,193))+list(range(199,209))
-    print(pro_res)
+    
+    prot_scaler = s.restraints.create_scaler('constant')              # defining a constant distance scaler i.e. it will keep restraint strength equal through the replica ladder
+    # prot_pep_scaler = s.restraints.create_scaler('nonlinear', alpha_min=0.4, alpha_max=1.0, factor=4.0)   # Defining a nonlinear distance scaler. 1st to 12th replica will have maximum restraint strength and then from 12 to 30th it will decreas making 0 at the 30th
+
+
+    interface = get_dist_restraints('contact.dat',s,scaler=prot_scaler)  # Enforcing binding restraints with non-linear scaler assignig high temperature replicas weaker restraints so that they can explore the energy landscape.
+    s.restraints.add_selectively_active_collection(interface, int(len(interface)*1.00))   # Trusting all the groups in the restraint file
+
+    prot_rest = get_dist_restraints_protein('protein_contacts.dat',s,scaler=prot_scaler)        #Enforcing intra protein restraints with constant scaler so that it does not unfold.
+    s.restraints.add_selectively_active_collection(prot_rest, int(len(prot_rest)*0.90))   
+    
+    
+    # const_scaler = s.restraints.create_scaler('constant')
+    # pro_res = list(range(26,91)) + list(range(121,151))+list(range(154,163))+list(range(165,193))+list(range(199,209))
+    # print(pro_res)
     # Keep protein close to starting conformation
-    rest = make_cartesian_collections(s, const_scaler, pro_res)
-    s.restraints.add_as_always_active_list(rest)
+    # rest = make_cartesian_collections(s, const_scaler, pro_res)
+    # s.restraints.add_as_always_active_list(rest)
  
     # create the options
     options = system.RunOptions()
